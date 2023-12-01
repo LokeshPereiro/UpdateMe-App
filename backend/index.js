d205253c75706a86ebb74f94ca2fe78cd361cee7
Lokesh Pereiro <lpdraft001@gmail.com>
2023-12-01 15:44:39 +0100

    develop branch

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,16 @@ import { dbConnection } from "./db/dbConfig.js";
 import authRoutes from "./routes/auth.js";
 import { register } from "./controllers/auth.js";
 
+import userRoutes from "./routes/users.js";
+
+import postRoutes from "./routes/posts.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
+
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
+
 /* Middlewares and Packages Config */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename); //As we using type:"module", this will help us to trigger local dir paths
@@ -40,9 +50,12 @@ const upload = multer({ storage });
 
 /* Routes Middleware */
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* Routes */
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+app.use("/posts", postRoutes);
 
 const port = process.env.PORT || 5000;
 
@@ -50,6 +63,9 @@ const start = async () => {
   try {
     await dbConnection(process.env.MONGO_URI);
     app.listen(port, console.log(`~Server running on port ${port}...`));
+    /* ADD DATA ONE TIME */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   } catch (error) {
     console.log(error);
   }
